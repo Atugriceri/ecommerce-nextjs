1afbb9d28ecdddd026d72c5338882cb90bbd0e8b
Alper Tugriçeri <alpertugriceri@gmail.com>
2022-04-28 19:24:40 +0300

    providers added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { ProductProvider } from '../contexts/ProductContext'
+import { FilterProvider } from '../contexts/FilterContext'
+import { BreadcrumbProvider } from '../contexts/BreadcrumbContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ProductProvider>
+      <FilterProvider>
+        <BreadcrumbProvider>
+          <Component {...pageProps} />
+        </BreadcrumbProvider>
+      </FilterProvider>
+    </ProductProvider>
+  )
 }
 
 export default MyApp
