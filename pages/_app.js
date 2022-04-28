e297ef18fcf77ac87c892c6004af415b575572dc
Alper Tugriçeri <alpertugriceri@gmail.com>
2022-04-29 02:23:53 +0300

    import bootstrap icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { ProductProvider } from '../contexts/ProductContext'
 import { FilterProvider } from '../contexts/FilterContext'
 import { BreadcrumbProvider } from '../contexts/BreadcrumbContext'
