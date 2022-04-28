b69add5065c9400a11701f9a2cd6633d369f4536
Alper Tugriçeri <alpertugriceri@gmail.com>
2022-04-29 02:26:23 +0300

    created Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <header>
+    <header className="border-bottom border-2">
       <div className="container">
-      <Image src="/logo.svg" alt="Vercel Logo" width={113} height={34} />
+        <div className="row py-3 justify-content-between align-items-center">
+          <div className="col d-flex align-items-center">
+            <Link href="/">
+              <Image src="/logo.svg" alt="Morhipo logo" title="Morhipo" width={113} height={34} />
+            </Link>
+          </div>
+          <div className="col d-flex justify-content-end">
+            <Link href="/">
+              Sepetim
+            </Link>
+          </div>
+        </div>  
       </div>
-
     </header>
   )
 }
